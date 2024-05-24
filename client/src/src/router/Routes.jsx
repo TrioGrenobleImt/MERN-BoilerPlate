987b0304cf905be2d588ddbf0ee971e0473eca64
Téo <teo.villet2@gmail.com>
2024-05-24 18:18:21 +0200

    Add auth routes to the router

--- client/src/src/router/Routes.jsx
+++ client/src/src/router/Routes.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home/Home'
+import Login from '../pages/Authentication/Login'
+import Register from '../pages/Authentication/Register'
 
 export const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/*' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
       </Routes>
     </BrowserRouter>
   )
