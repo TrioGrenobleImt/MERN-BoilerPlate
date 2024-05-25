c55aaa3ff0920d2d4a6335154abd99ca768dede4
Téo <teo.villet2@gmail.com>
2024-05-25 20:39:15 +0200

    Update route and pre check if user is connected to control access

--- client/src/router/Routes.jsx
+++ client/src/router/Routes.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Home from '../pages/Home/Home'
 import Login from '../pages/Authentication/Login'
 import Register from '../pages/Authentication/Register'
+import Account from '../pages/Account/Account'
 
 export const Router = () => {
+  //Implement a user authentication check
+  const user = true
   return (
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path='/login' element={<Login />} />
-      <Route path='/register' element={<Register />} />
+      <Route path='/register' element={user ? <Navigate to={'/'} /> : <Register />} />
+      <Route path='/login' element={user ? <Navigate to={'/'} /> : <Login />} />
+      <Route path='/account' element={!user ? <Navigate to={'/login'} /> : <Account />} />
     </Routes>
   )
 }
