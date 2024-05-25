03f4c4c3bbf99efba3ea4665a6a1e16625b8cd9e
Téo <teo.villet2@gmail.com>
2024-05-25 16:17:08 +0200

    Add toast library

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter } from 'react-router-dom'
 import NavBar from './components/NavBar.jsx'
 import { Router } from './router/Routes.jsx'
+import { Toaster } from 'react-hot-toast'
 
 function App() {
   return (
     <BrowserRouter>
       <NavBar />
       <Router />
+      <Toaster />
     </BrowserRouter>
   )
 }
