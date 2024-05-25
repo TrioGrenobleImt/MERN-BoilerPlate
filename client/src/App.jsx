3279a289c89485535a3a256a2d1d0ab59f906f8f
Téo <teo.villet2@gmail.com>
2024-05-25 23:44:20 +0200

    Optimize app by sendin the BrowserRouter and the Toaster into the app file Get the AuthContextProvider in the app too

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,12 @@
-import { BrowserRouter } from 'react-router-dom'
 import NavBar from './components/NavBar.jsx'
 import { Router } from './router/Routes.jsx'
-import { Toaster } from 'react-hot-toast'
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <NavBar />
       <Router />
-      <Toaster />
-    </BrowserRouter>
+    </>
   )
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './style/index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
+import { AuthContextProvider } from './contexts/authContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
+      <Toaster />
+    </BrowserRouter>
   </React.StrictMode>
 )
