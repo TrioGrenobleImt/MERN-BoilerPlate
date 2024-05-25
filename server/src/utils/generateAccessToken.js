cdea8ea3d8fe2cb64f0f347ed1754065a5ff25af
Téo <teo.villet2@gmail.com>
2024-05-25 22:56:55 +0200

    Im stupid its not a route but a method...

--- server/src/utils/generateAccessToken.js
+++ server/src/utils/generateAccessToken.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken'
 
-export const generateAccessToken = (req, res) => {
-  return jwt.sign({ id: req.body }, process.env.SECRET_ACCESS_TOKEN, { expiresIn: '30d' })
+export const generateAccessToken = (userId) => {
+  return jwt.sign({ id: userId }, process.env.SECRET_ACCESS_TOKEN, { expiresIn: '30d' })
 }
