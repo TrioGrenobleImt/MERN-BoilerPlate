39e403e23309990b8975006434918371e1e31ac3
Téo <teo.villet2@gmail.com>
2024-05-25 16:18:45 +0200

    Add login form and utilisation of the hooks

--- client/src/pages/Authentication/Login.jsx
+++ client/src/pages/Authentication/Login.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useLogin } from '../../hooks/useLogin'
 
 const Login = () => {
-  return <div>Hello from Login</div>
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const { loading, login } = useLogin()
+
+  const handleLogin = async (e) => {
+    e.preventDefault()
+    await login({ username, password })
+  }
+
+  return (
+    <>
+      <h1>Hello from login</h1>
+      <form action='submit'>
+        <input type='text' value={username} onChange={(e) => setUsername(e.target.value)} placeholder='Username' />
+        <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Password' />
+        <button type='submit' onClick={handleLogin} disabled={loading}>
+          Login
+        </button>
+      </form>
+    </>
+  )
 }
 
 export default Login
