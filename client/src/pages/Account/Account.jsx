cc2eab3080cf4f188a719da10cf927de246ca880
Téo <teo.villet2@gmail.com>
2024-05-29 17:38:51 +0200

    Create account page with logout btn

--- client/src/pages/Account/Account.jsx
+++ client/src/pages/Account/Account.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import { useLogout } from '../../hooks/useLogout'
 
 const Account = () => {
-  return <div>Hello from account</div>
+  const { logout, loading } = useLogout()
+
+  const handleClick = async (e) => {
+    e.preventDefault()
+    await logout()
+  }
+
+  return (
+    <div>
+      <h1>Hello from Account</h1>
+      <button onClick={handleClick} disabled={loading}>
+        Logout
+      </button>
+    </div>
+  )
 }
 
 export default Account
