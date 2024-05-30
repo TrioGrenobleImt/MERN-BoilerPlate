406929c1618489b20ba611a3dd357f31e3ee1ced
Téo <teo.villet2@gmail.com>
2024-05-30 18:04:26 +0200

    Improve authContext : Connected -> getUserData Not connected -> doesnt do the request

--- client/src/contexts/authContext.jsx
+++ client/src/contexts/authContext.jsx
@@ -12,11 +12,16 @@ export const AuthContextProvider = ({ children }) => {
 
   useEffect(() => {
     const getAuthUser = async () => {
-      //Ne faire la requete que si le token est présent dans les cookies
       try {
-        const response = await axiosConfig('/auth/me')
-        if (response.data) {
-          setAuthUser(response.data)
+        const response = await axiosConfig.get('/auth/check')
+        const isAuthenticated = response.data.authenticated
+
+        if (isAuthenticated) {
+          const userResponse = await axiosConfig.get('/auth/me')
+          const userData = userResponse.data
+          setAuthUser(userData)
+        } else {
+          setAuthUser(null)
         }
       } catch (error) {
         setAuthUser(null)
