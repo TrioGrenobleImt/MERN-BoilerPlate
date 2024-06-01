88f876331baa892c793c7f52f06d449bdcadd3f2
Téo <teo.villet2@gmail.com>
2024-06-01 15:18:50 +0200

    Add request to register into hook

---
+++ client/src/hooks/useRegister.js
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+import axiosConfig from '../config/axiosConfig'
+import { useNavigate } from 'react-router-dom'
+import toast from 'react-hot-toast'
+import { useAuthContext } from '../contexts/authContext'
+
+export const useRegister = () => {
+  const [loading, setLoading] = useState(false)
+  const navigate = useNavigate()
+  const { authUser, setAuthUser } = useAuthContext()
+
+  const register = async ({ username, password, confirmPassword, email }) => {
+    const success = handleInputsError(username, password, confirmPassword, email)
+    if (!success) return
+    setLoading(true)
+    try {
+      //Requete
+
+      const response = await axiosConfig.post('/auth/register', {
+        username,
+        password,
+        confirmPassword,
+        email,
+      })
+
+      const data = await response.data
+      if (data.error) {
+        throw new Error(data.error)
+      }
+
+      toast.success(data.message)
+
+      setAuthUser(data.user)
+      navigate('/')
+    } catch (error) {
+      toast.error(error.response.data.error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return { loading, register }
+}
+
+function handleInputsError(username, password, confirmPassword, email) {
+  if (!username || !password || !confirmPassword || !email) {
+    toast.error('Please fill in all fields')
+    return false
+  }
+
+  // Mettre regex mdp etc...
+
+  return true
+}
