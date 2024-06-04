120e2c48ba09bd52e8724adb8ef886acca1c1d66
Téo <teo.villet2@gmail.com>
2024-06-04 17:36:57 +0200

    Add template for authTest

---
+++ server/tests/authenticationController.test.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
+import request from 'supertest'
+
+beforeAll(async () => {
+  //Connect to database
+})
+
+afterAll(async () => {
+  //Discornnect from database
+})
