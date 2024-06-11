ed7926aede7ca18dd447e1281f5708504e533145
Téo <teo.villet2@gmail.com>
2024-06-11 13:49:30 +0200

    Add unit test for middleware

---
+++ server/tests/verifyToken.test.js
@@ -0,0 +1,48 @@
+import mongoose from 'mongoose'
+import { describe, it, beforeAll, afterAll, expect, afterEach, vitest } from 'vitest'
+import 'dotenv/config'
+import request from 'supertest'
+import jwt from 'jsonwebtoken'
+
+//Import app
+import app from '../server.js'
+
+beforeAll(async () => {
+  //Connect to database
+  await mongoose.connect(process.env.MONG_URI_TEST)
+})
+
+afterAll(async () => {
+  //Disconnect from database
+  await mongoose.disconnect()
+})
+
+describe('verifyToken', () => {
+  it('should return a 401 status error because the token is missing', async () => {
+    const res = await request(app).get('/api/auth/me').send()
+
+    expect(res.status).toBe(401)
+    expect(res.body.message).toBe('Not Authenticated')
+  })
+
+  it('should return a 403 status error because the token is invalid', async () => {
+    const res = await request(app).get('/api/auth/me').set('Cookie', '__access__token=invalidtoken').send()
+
+    expect(res.status).toBe(403)
+    expect(res.body.message).toBe('Access Token is invalid')
+  })
+
+  it('should call next middleware because the token is valid', async () => {
+    const user = { id: 'userId123' }
+    const token = jwt.sign(user, process.env.SECRET_ACCESS_TOKEN)
+
+    const res = await request(app)
+      .get('/some-protected-route') // Assurez-vous que cette route utilise le middleware verifyToken
+      .set('Cookie', `__access__token=${token}`)
+      .send()
+
+    // We expect a 404 because the route '/some-protected-route' doesn't exist
+    // If the token is valid, it should pass the middleware and look for the next middleware or route handler
+    expect(res.status).toBe(404)
+  })
+})
