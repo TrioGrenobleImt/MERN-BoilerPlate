8a048c9e7861f6b339a999b038b64afe29248a43
Téo <teo.villet2@gmail.com>
2024-06-11 13:49:52 +0200

    Add unit test for DB

---
+++ server/tests/connectToDB.test.js
@@ -0,0 +1,45 @@
+import { describe, it, beforeEach, afterEach, expect, vi } from 'vitest'
+import sinon from 'sinon'
+import mongoose from 'mongoose'
+import { connectToDatabase } from '../src/database/connectToDB'
+
+describe('connectToDatabase', () => {
+  let connectStub
+
+  beforeEach(() => {
+    connectStub = sinon.stub(mongoose, 'connect')
+  })
+
+  afterEach(() => {
+    connectStub.restore()
+  })
+
+  it('should call mongoose.connect and log success message on successful connection', async () => {
+    connectStub.resolves()
+
+    const consoleLogSpy = vi.spyOn(console, 'log')
+
+    await connectToDatabase()
+
+    expect(connectStub.calledOnce).toBe(true)
+    expect(connectStub.calledWith(process.env.MONG_URI)).toBe(true)
+    expect(consoleLogSpy).toHaveBeenCalledWith('Connected to the database 🧰')
+
+    consoleLogSpy.mockRestore()
+  })
+
+  it('should call mongoose.connect and log error message on connection failure', async () => {
+    const error = new Error('Connection error')
+    connectStub.rejects(error)
+
+    const consoleErrorSpy = vi.spyOn(console, 'error')
+
+    await connectToDatabase()
+
+    expect(connectStub.calledOnce).toBe(true)
+    expect(connectStub.calledWith(process.env.MONG_URI)).toBe(true)
+    expect(consoleErrorSpy).toHaveBeenCalledWith(error)
+
+    consoleErrorSpy.mockRestore()
+  })
+})
