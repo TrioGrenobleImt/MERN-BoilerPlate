af6b0b992b472e2bb2c6d97ec46b1148b62bf326
teovlt <teo.villet2@gmail.com>
2025-03-05 22:12:33 +0100

    Add filter for getLogs

--- server/src/controllers/logController.js
+++ server/src/controllers/logController.js
@@ -2,8 +2,16 @@ import Log from "../models/logModel.js";
 import { logLevels } from "../utils/enums/logLevel.js";
 
 const getLogs = async (req, res) => {
+  const { size } = req.body;
   try {
-    const logs = await Log.find().populate("user", "-password");
+    const query = Log.find().populate("user", "-password").sort({ createdAt: -1 });
+
+    if (size && !isNaN(size)) {
+      query.limit(Number(size));
+    }
+
+    const logs = await query;
+
     res.status(200).json(logs);
   } catch (err) {
     res.status(500).json({ error: err.message });
