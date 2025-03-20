493816aa07c6a32e02a35095e4b244185b1caa9f
teovlt <teo.villet2@gmail.com>
2025-03-20 19:16:14 +0100

    Add random color when creating user

---
+++ server/src/routes/uploadRoutes.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/avatar/default", async (req, res) => {
+  const color = decodeURIComponent(req.query.color);
+  console.log(color);
+  const svg = `
+    <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="50" cy="50" r="50" fill="${color}" />
+    <circle cx="50" cy="40" r="15" fill="white" />
+    <rect x="30" y="60" width="40" height="25" rx="10" fill="white" />
+    </svg>
+    `;
+  res.set("Content-Type", "image/svg+xml");
+  res.send(svg);
+});
+
+export default router;
