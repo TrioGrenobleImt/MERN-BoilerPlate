5a43bb197f56ccf012facfbffe5f1f5b581517a4
teovlt <teo.villet2@gmail.com>
2025-03-25 11:17:28 +0100

    Ajout du file filter

--- server/src/configuration/storageConfig.js
+++ server/src/configuration/storageConfig.js
@@ -1,11 +1,32 @@
 import multer from "multer";
 
-export const storageConfig = multer.diskStorage({
+// Configuration de stockage
+const storageConfig = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "./uploads/users/avatars");
   },
   filename: function (req, file, cb) {
     const extension = file.originalname.split(".").pop();
-    cb(null, `avatar_${req.userId}_${Date.now()}${extension ? `.${extension}` : ""}`);
+    cb(null, `avatar_${req.userId}_${Date.now()}.${extension}`);
   },
 });
+
+// Filtre de fichiers
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = /jpg|jpeg|png|gif|svg/;
+  const isValid = allowedTypes.test(file.mimetype);
+  if (!isValid) {
+    const error = new Error("Invalid file type. Only jpg, jpeg, png, gif, and svg are allowed.");
+    error.statusCode = 400;
+    return cb(error, false);
+  }
+  cb(null, true);
+};
+
+const uploadConfig = multer({
+  storage: storageConfig,
+  fileFilter: fileFilter,
+  limits: { fileSize: 5 * 1024 * 1024 }, // Limite à 5 Mo (5 * 1024 * 1024 bytes)
+});
+
+export default uploadConfig;
