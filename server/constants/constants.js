5184c1f6b42cfb75eca76c6547ff6ac8bb0e1c04
teovlt <teo.villet2@gmail.com>
2025-03-26 19:47:15 +0100

    Move avatar_size_limit into constants file

--- server/constants/constants.js
+++ server/constants/constants.js
@@ -4,7 +4,9 @@
  * This object includes constants such as expiration times, limits, and other fixed values.
  * @type {Object}
  * @property {number} MAX_AGE - The maximum age (in milliseconds) for tokens or sessions, set to 30 days.
+ * @property {number} AVATAR_MAX_SIZE - The maximum size (in bytes) for an avatar file, set to 3 MB.
  */
 export const Constants = {
   MAX_AGE: 30 * 24 * 60 * 60 * 1000,
+  AVATAR_MAX_SIZE: 3 * 1024 * 1024,
 };
