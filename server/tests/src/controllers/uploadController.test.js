08135641458b9f325c8e4201f26b5e820773ff88
teovlt <teo.villet2@gmail.com>
2025-03-28 20:11:11 +0100

    Move storage error into controller too

--- server/tests/src/controllers/uploadController.test.js
+++ server/tests/src/controllers/uploadController.test.js
@@ -9,6 +9,7 @@ import { generateAccessToken } from "../../../src/utils/generateAccessToken.js";
 
 //Import server and app
 import app from "../../../src/app.js";
+import { Constants } from "../../../constants/constants.js";
 
 beforeAll(async () => {
   //Connect to database
@@ -31,7 +32,7 @@ afterAll(async () => {
   }
 });
 
-describe("Tests configuration files", () => {
+describe("Tests uploads files", () => {
   afterEach(async () => {
     await User.deleteMany();
   });
@@ -80,6 +81,41 @@ describe("Tests configuration files", () => {
     }
   });
 
+  it("should return a 500 error if there is a server problem", async () => {
+    const pathAvatarOld = "./uploads/users/avatars/hello-world.png";
+    fs.writeFileSync(pathAvatarOld, "Hello, world!");
+
+    const user = await User.create({
+      username: "test",
+      email: "test@gmail.com",
+      password: "test",
+      role: "admin",
+      name: "test",
+      forename: "test",
+      avatar: pathAvatarOld,
+    });
+
+    vi.spyOn(User, "findById").mockImplementationOnce(() => {
+      throw new Error("Test error");
+    });
+
+    const pathNewAvatar = "./tests/src/controllers/hello-world.png";
+    fs.writeFileSync(pathNewAvatar, "Hello, world!");
+
+    const response = await request(app)
+      .post(`/api/uploads/avatar/${user._id}`)
+      .set("Cookie", `__access__token=${generateAccessToken(user._id)}`)
+      .attach("avatar", pathNewAvatar, "hello-world.png");
+
+    expect(response.body.error).toBe("An unexpected error occurred during file upload");
+    expect(response.statusCode).toBe(500);
+
+    // Nettoyage des fichiers temporaires
+    if (fs.existsSync(pathNewAvatar)) {
+      fs.unlinkSync(pathNewAvatar);
+    }
+  });
+
   it("should delete old profilePic if there is one and update the current", async () => {
     const pathAvatarOld = "./uploads/users/avatars/hello-world.png";
     fs.writeFileSync(pathAvatarOld, "Hello, world!");
@@ -114,7 +150,7 @@ describe("Tests configuration files", () => {
     }
   });
 
-  it("should delete old profilePic if there is one and update the current", async () => {
+  it("should return an error if the file is too large", async () => {
     const pathAvatarOld = "./uploads/users/avatars/hello-world.png";
     fs.writeFileSync(pathAvatarOld, "Hello, world!");
 
@@ -128,20 +164,18 @@ describe("Tests configuration files", () => {
       avatar: pathAvatarOld,
     });
 
-    vi.spyOn(User, "findById").mockImplementationOnce(() => {
-      throw new Error("Test error");
-    });
-
     const pathNewAvatar = "./tests/src/controllers/hello-world.png";
-    fs.writeFileSync(pathNewAvatar, "Hello, world!");
+    const fileSizeInBytes = 10 * 1024 * 1024;
+    const data = "A".repeat(fileSizeInBytes); // Répéter "A" jusqu'à atteindre la taille du fichier
+    fs.writeFileSync(pathNewAvatar, data);
 
     const response = await request(app)
       .post(`/api/uploads/avatar/${user._id}`)
       .set("Cookie", `__access__token=${generateAccessToken(user._id)}`)
       .attach("avatar", pathNewAvatar, "hello-world.png");
 
-    expect(response.body.error).toBe("An unexpected error occurred during file upload");
-    expect(response.statusCode).toBe(500);
+    expect(response.statusCode).toBe(400);
+    expect(response.body.error).toBe(`File size exceeds the limit of ${Constants.AVATAR_MAX_SIZE / 1024 / 1024} MB`);
 
     // Nettoyage des fichiers temporaires
     if (fs.existsSync(pathNewAvatar)) {
