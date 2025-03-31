16a9e8ea14b479d5c09c2b36c33e15c7e040e111
teovlt <teo.villet2@gmail.com>
2025-03-31 13:06:06 +0200

    Refactor -> move socket config into its own file

---
+++ server/src/sockets/socket.js
@@ -0,0 +1,29 @@
+import { Server as SocketIOServer } from "socket.io";
+import { corsOptions } from "../configuration/corsOptions.js";
+
+let io;
+const userSocketMap = {};
+
+export function initSockets(httpServer) {
+  io = new SocketIOServer(httpServer, {
+    cors: corsOptions,
+  });
+
+  io.on("connection", (socket) => {
+    const userId = socket.handshake.query.userId;
+
+    if (userId) {
+      userSocketMap[userId] = socket.id;
+      io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    }
+
+    socket.on("disconnect", () => {
+      if (userId) {
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
+      }
+    });
+  });
+}
+
+export { io };
