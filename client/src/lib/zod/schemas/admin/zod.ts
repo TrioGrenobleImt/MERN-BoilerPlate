aa5e9f560289807df6b4614d9a47355c97901e56
teovlt <teo.villet2@gmail.com>
2025-04-08 10:31:04 +0200

    Fix display of error message for creating user without password

--- client/src/lib/zod/schemas/admin/zod.ts
+++ client/src/lib/zod/schemas/admin/zod.ts
@@ -14,7 +14,7 @@ export const createPlayerSchema = z.object({
     .min(2, { message: "Username must be at least 2 characters long" })
     .max(25, { message: "Username must be at most 25 characters long" }),
   email: z.string().email({ message: "Invalid email address" }),
-  password: z.string().max(255, { message: "Password must be at most 255 characters long" }),
+  password: z.string().min(1, { message: "Password is required" }).max(255, { message: "Password must be at most 255 characters long" }),
   role: z.string(),
 });
 
