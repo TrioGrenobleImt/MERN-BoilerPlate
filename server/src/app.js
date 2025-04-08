309246ba6d6b526feaf804732376b7945fa34754
teovlt <teo.villet2@gmail.com>
2025-04-08 21:08:36 +0200

    Add basic email with resend API KEY

--- server/src/app.js
+++ server/src/app.js
@@ -7,6 +7,8 @@ import cookieParser from "cookie-parser";
 import { corsOptions } from "./configuration/corsOptions.js";
 import { router } from "./routes/router.js";
 
+import { Resend } from "resend";
+
 //express app
 export const app = express();
 
@@ -19,6 +21,28 @@ app.use((req, res, next) => {
 });
 app.use(cors(corsOptions));
 app.use(cookieParser());
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+if (!process.env.RESEND_API_KEY) {
+  console.error("Missing RESEND_API_KEY environment variable");
+  process.exit(1);
+}
+
+app.get("/email", async (req, res) => {
+  const { data, error } = await resend.emails.send({
+    from: "MERN-Boilerplate <no-reply-MERN-Boilerplate@resend.dev>",
+    to: ["teo.villet2@gmail.com"],
+    subject: "hello world",
+    html: "<strong>it works!</strong>",
+  });
+
+  if (error) {
+    return res.status(400).json({ error: error.message });
+  }
+
+  res.status(200).json({ message: "Email sent successfully" });
+});
 
 // Routes
 app.use(router);
