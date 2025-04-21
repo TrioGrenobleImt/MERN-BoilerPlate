f936be3116b34f040e67c093b354a3b5c7e27657
teovlt <teo.villet2@gmail.com>
2025-04-21 11:12:15 +0200

    Add createConfig method

--- server/src/controllers/configController.js
+++ server/src/controllers/configController.js
@@ -9,7 +9,21 @@ export const getConfig = async (req, res) => {
   }
 };
 
-export const createConfig = async (req, res) => {};
+export const createConfig = async (req, res) => {
+  const { key, value } = req.body;
+
+  if (!key || !value) {
+    return res.status(400).json({ error: "Missing fields" });
+  }
+
+  try {
+    const config = await Config.create({ key, value });
+
+    res.status(201).json({ config });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
 
 export const updateConfig = async (req, res) => {};
 
