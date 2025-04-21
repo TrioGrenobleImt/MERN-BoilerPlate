07f8f8aa244599ed307a90909f1da5669ace5220
teovlt <teo.villet2@gmail.com>
2025-04-21 15:50:13 +0200

    Update method to get all the values of each key of the config documents

--- server/src/routes/configRoute.js
+++ server/src/routes/configRoute.js
@@ -1,11 +1,6 @@
 import express from "express";
-import { getConfig, createConfig, updateConfig, deleteConfig } from "../controllers/configController.js";
-import { verifyToken } from "../middlewares/verifyToken.js";
+import { getConfig } from "../controllers/configController.js";
 
 export const configRouter = new express.Router();
 
-configRouter.get("/", verifyToken({ role: "admin" }), getConfig);
-
-configRouter.post("/", verifyToken({ role: "admin" }), createConfig);
-configRouter.put("/", verifyToken({ role: "admin" }), updateConfig);
-configRouter.delete("/", verifyToken({ role: "admin" }), deleteConfig);
+configRouter.get("/", getConfig);
