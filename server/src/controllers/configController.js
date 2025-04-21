7bd0c57a83a855566cdc314eb52ec899d05b1506
teovlt <teo.villet2@gmail.com>
2025-04-21 16:08:16 +0200

    Add updateconfig method

--- server/src/controllers/configController.js
+++ server/src/controllers/configController.js
@@ -12,3 +12,24 @@ export const getConfig = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const updateConfig = async (req, res) => {
+  const { keys } = req.body;
+
+  if (!keys || Object.keys(keys).length === 0) {
+    return res.status(400).json({ message: "No keys to update" });
+  }
+
+  try {
+    const updatePromises = Object.keys(keys).map(async (key) => {
+      const updatedConfig = await Config.findOneAndUpdate({ key }, { value: keys[key] }, { new: true });
+      return updatedConfig;
+    });
+
+    const updatedConfigs = await Promise.all(updatePromises);
+
+    res.json({ message: "Configuration updated successfully", config: updatedConfigs });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- server/src/routes/configRoute.js
+++ server/src/routes/configRoute.js
@@ -1,6 +1,7 @@
 import express from "express";
-import { getConfig } from "../controllers/configController.js";
+import { getConfig, updateConfig } from "../controllers/configController.js";
 
 export const configRouter = new express.Router();
 
 configRouter.get("/", getConfig);
+configRouter.put("/", updateConfig);
