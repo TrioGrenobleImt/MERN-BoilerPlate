26420fc608c225a290474aa0c0b837e1aae17550
teovlt <teo.villet2@gmail.com>
2025-04-22 11:32:34 +0200

    Delete LOGO_URL conf

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -19,7 +19,7 @@ const GlobalConfigLoader = () => {
 
   useEffect(() => {
     if (!isLoaded) {
-      loadConfig(["APP_NAME", "LOGO_URL"]);
+      loadConfig(["APP_NAME"]);
     }
   }, [isLoaded, loadConfig]);
 
