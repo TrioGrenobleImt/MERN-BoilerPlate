4e6dc2e6f09df93490eb541dbf30bade772002c8
VILLET Téo <97348025+teovlt@users.noreply.github.com>
2025-05-20 15:51:24 +0200

    Update constants.js

--- server/constants/constants.js
+++ server/constants/constants.js
@@ -11,5 +11,5 @@
 export const Constants = {
   MAX_AGE: 30 * 24 * 60 * 60,
   AVATAR_MAX_SIZE: 3 * 1024 * 1024,
-  REGEX_PASSWORD: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&:.]).{8,}$/,
+  REGEX_PASSWORD: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9]).{8,}$/
 };
