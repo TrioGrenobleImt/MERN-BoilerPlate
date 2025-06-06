b15de9fbaa6a9dd7c8d04513f0168f4deb260d26
teovlt <teo.villet2@gmail.com>
2025-06-06 15:20:51 +0200

    Set all firebase keys and tokens into the .env

--- client/src/lib/firebase.ts
+++ client/src/lib/firebase.ts
@@ -6,11 +6,11 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "mern-boilerplate-1afcb.firebaseapp.com",
-  projectId: "mern-boilerplate-1afcb",
-  storageBucket: "mern-boilerplate-1afcb.firebasestorage.app",
-  messagingSenderId: "987053996490",
-  appId: "1:987053996490:web:13c870b6e6a94baa81b733",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
