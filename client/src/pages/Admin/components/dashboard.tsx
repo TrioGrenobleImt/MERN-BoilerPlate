876ba1727ebed829547b662cddf71ececef33fdf
teovlt <teo.villet2@gmail.com>
2025-06-18 21:31:01 +0200

    Add info into dashboard for auth_type but false data

--- client/src/pages/Admin/components/dashboard.tsx
+++ client/src/pages/Admin/components/dashboard.tsx
@@ -2,15 +2,19 @@ import { Loading } from "@/components/customs/loading";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import { axiosConfig } from "@/config/axiosConfig";
 import { useSocketContext } from "@/contexts/socketContext";
-import { Activity, Users } from "lucide-react";
+import { Activity, Users, BarChart3 } from "lucide-react";
 import { useEffect, useState } from "react";
 import { toast } from "sonner";
+import { PieChart, pieArcLabelClasses } from "@mui/x-charts/PieChart";
 
 export const Dashboard = () => {
   const [loading, setLoading] = useState(false);
   const [userCount, setUserCount] = useState(0);
-
   const { onlineUsers } = useSocketContext();
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
 
   async function fetchUsers() {
     setLoading(true);
@@ -24,32 +28,67 @@ export const Dashboard = () => {
     }
   }
 
-  useEffect(() => {
-    fetchUsers();
-  }, []);
+  const chartData = {
+    data: [
+      { label: "Google", value: 72 },
+      { label: "Local", value: 28 },
+    ],
+    valueFormatter,
+  };
+
   return (
     <>
       {loading ? (
         <Loading />
       ) : (
         <div className="flex flex-col px-4 space-y-4 md:space-y-6 md:px-8">
-          <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-            <Card>
-              <CardHeader className="flex flex-row items-center justify-between pb-2 space-y-0">
+          <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-2">
+            <Card className="max-h-[120px]">
+              <CardHeader className="flex flex-row items-center justify-between pb-2">
                 <CardTitle className="text-sm font-medium">Active users</CardTitle>
                 <Activity className="w-4 h-4 text-accent" />
               </CardHeader>
               <CardContent>
-                <div className="text-2xl font-bold"> + {onlineUsers.length}</div>
+                <div className="text-2xl font-bold">+ {onlineUsers.length}</div>
               </CardContent>
             </Card>
-            <Card>
-              <CardHeader className="flex flex-row items-center justify-between pb-2 space-y-0">
+
+            <Card className="max-h-[120px]">
+              <CardHeader className="flex flex-row items-center justify-between pb-2">
                 <CardTitle className="text-sm font-medium">Registrations</CardTitle>
                 <Users className="w-4 h-4 text-accent" />
               </CardHeader>
               <CardContent>
-                <div className="text-2xl font-bold"> + {userCount}</div>
+                <div className="text-2xl font-bold">+ {userCount}</div>
+              </CardContent>
+            </Card>
+
+            <Card className="lg:col-span-2">
+              <CardHeader className="flex flex-row items-center justify-between pb-2">
+                <CardTitle className="text-sm font-medium">Auth methods</CardTitle>
+                <BarChart3 className="w-4 h-4 text-accent" />
+              </CardHeader>
+              <CardContent className="flex items-center justify-center h-[200px]">
+                <PieChart
+                  series={[
+                    {
+                      arcLabel: (item: any) => `${item.value}%`,
+                      arcLabelMinAngle: 25,
+                      arcLabelRadius: "60%",
+                      ...chartData,
+                      color: "#4f46e5",
+                    },
+                  ]}
+                  sx={{
+                    [`& .${pieArcLabelClasses.root}`]: {
+                      fontWeight: "bold",
+                      fontSize: 15,
+                      fill: "#fff",
+                    },
+                  }}
+                  width={300}
+                  height={200}
+                />
               </CardContent>
             </Card>
           </div>
@@ -58,3 +97,5 @@ export const Dashboard = () => {
     </>
   );
 };
+
+export const valueFormatter = (item: { value: number }) => `${item.value}%`;
