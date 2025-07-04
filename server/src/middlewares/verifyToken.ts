6057dd68718fe24ce4b4817e6bacb26773d41bd0
teovlt <teo.villet2@gmail.com>
2025-07-04 17:52:16 +0200

    Basically im done with the cookies -> localStorage on top

--- server/src/middlewares/verifyToken.ts
+++ server/src/middlewares/verifyToken.ts
@@ -20,13 +20,11 @@ interface VerifyTokenOptions {
  */
 export const verifyToken = ({ role }: VerifyTokenOptions = {}) => {
   return async (req: Request, res: Response, next: NextFunction) => {
-    const token = req.cookies["__access__token"];
-    if (!token) {
-      res.status(401).json({ error: "global.expired_session" });
-      return;
-    }
+    const authHeader = req.headers.authorization;
+    if (!authHeader) return res.status(401).json({ error: "Not Authenticated" });
 
     try {
+      const token = authHeader.split(" ")[1];
       const decoded = jwt.verify(token, process.env.SECRET_ACCESS_TOKEN as string) as TokenPayload;
 
       const user = await User.findById(decoded.id);
