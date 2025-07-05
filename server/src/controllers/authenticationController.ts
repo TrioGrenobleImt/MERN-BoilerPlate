2d7598707a351720a5ec9ba74461567e71ba1eaa
teovlt <teo.villet2@gmail.com>
2025-07-05 11:16:36 +0200

    Fix authController tests

--- server/src/controllers/authenticationController.ts
+++ server/src/controllers/authenticationController.ts
@@ -141,11 +141,7 @@ export const login = async (req: Request, res: Response): Promise<void> => {
  * Logs out a user by clearing the access token cookie.
  */
 export const logout = async (req: Request, res: Response): Promise<void> => {
-  try {
-    res.status(200).json({ message: "server.auth.messages.logout_success" });
-  } catch (err: any) {
-    res.status(500).json({ error: err.message });
-  }
+  res.status(200).json({ message: "server.auth.messages.logout_success" });
 };
 
 /**
