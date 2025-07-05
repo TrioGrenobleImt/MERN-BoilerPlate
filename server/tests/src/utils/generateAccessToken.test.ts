ffb6ae79d35894bd8691a3d0246a4bfb0aa70642
teovlt <teo.villet2@gmail.com>
2025-07-05 11:45:38 +0200

    one to go

--- server/tests/src/utils/generateAccessToken.test.ts
+++ server/tests/src/utils/generateAccessToken.test.ts
@@ -4,12 +4,16 @@ import mongoose from "mongoose";
 
 describe("Generate access token method", () => {
   it("should return an error if the access token secret is not specified", () => {
-    const exitSpy = vi.spyOn(process, "exit").mockImplementation({} as any);
+    const exitSpy = vi.spyOn(process, "exit").mockImplementation(() => {
+      throw new Error("fake exit");
+    });
     const consoleErrorSpy = vi.spyOn(console, "error");
 
     delete process.env.SECRET_ACCESS_TOKEN;
 
-    generateAccessToken(new mongoose.Types.ObjectId());
+    expect(() => {
+      generateAccessToken(new mongoose.Types.ObjectId());
+    }).toThrow("fake exit");
 
     expect(consoleErrorSpy).toHaveBeenCalledWith("Please specify the access token secret in the .env file.");
     expect(exitSpy).toHaveBeenCalledWith(1);
